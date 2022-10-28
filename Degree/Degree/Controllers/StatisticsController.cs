346dccac7378a5e716609aff3614d58546f59d27
Dimych26 <dimayovko@gmail.com>
2022-10-28 21:13:51 +0300

    - feat: telegram stats

---
+++ Degree/Degree/Controllers/StatisticsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Degree.Controllers
+{
+    public class StatisticsController : Controller
+    {
+        public IActionResult Telegram()
+        {
+            return View();
+        }
+    }
+}
